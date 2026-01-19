f9cfe84d338b293203759a7dc6183eb7c8d83e0e
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-19 20:03:29 -0300

    learning comments in C_19-01_20:03

---
+++ Comments/multiLineComment.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    /* This is a 
+    Multi-Line Comment*/
+    printf("Hello World!");
+}
---
+++ Comments/singleLineComment.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    //this is a comment
+    printf("Hello World!"); //this is a comment
+}
