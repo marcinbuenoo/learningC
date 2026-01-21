4724559773626b47a131b18869048de33a526801
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-21 19:38:17 -0300

    learning format specifiers_21-01_19:38

--- Variables/outputError.c
+++ Variables/outputError.c
@@ -1,5 +1,7 @@
 // You can't output a variable value using printf as you do when print texts
 #include <stdio.h>
 
-int myNum = 15;
-    printf(myNum);
+int main(){
+    int myNum = 15;
+        printf(myNum); // this will cause an error
+}
