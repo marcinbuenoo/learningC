12ad965cb12bf5f89b00c5d49ad2be4ee3612afb
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-01-22 17:58:46 -0300

    learning multiple declaring variable_22-01_17:58

---
+++ Statements/declareMultipleVariable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5, y = 6, z =50;
+        printf("%d\n", x + y + z);
+
+    int a, b, c;
+    a = b = c = 50;
+        printf("%d\n", a + b + c);
+    return 0;
+}
